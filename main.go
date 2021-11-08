98980a4c423a1d3da6212002d07c73339d34712e
Vladimir Totocevski <sephirothmk@gmail.com>
2021-11-08 11:56:42 +0100

    fixed sub

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 	"runtime"
 	"strings"
 
@@ -71,9 +72,18 @@ func create(ctx context.Context, projectID string, topics Topics) error {
 			return fmt.Errorf("Unable to create topic %q for project %q: %s", topicID, projectID, err)
 		}
 
+		subConfig := pubsub.SubscriptionConfig{
+                Topic: topic,
+                AckDeadline: 20 * time.Second,
+                DeadLetterPolicy: pubsub.DeadLetterPolicy{
+                        DeadLetterTopic: "inbound-dlq-topic",
+                        MaxDeliveryAttempts: 5,
+                },
+        }
+
 		for _, subscriptionID := range subscriptions {
 			debugf("    Creating subscription %q", subscriptionID)
-			_, err = client.CreateSubscription(ctx, subscriptionID, pubsub.SubscriptionConfig{Topic: topic})
+			_, err = client.CreateSubscription(ctx, subscriptionID, subConfig)
 			if err != nil {
 				return fmt.Errorf("Unable to create subscription %q on topic %q for project %q: %s", subscriptionID, topicID, projectID, err)
 			}
